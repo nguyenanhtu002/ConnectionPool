4d3c8ac1db8ce2f97b5cdf51a965b5aa00cc4129
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-06-13 10:32:33 +0700

    Update BookDAOImpl.java

--- QLS/src/main/java/dao/BookDAOImpl.java
+++ QLS/src/main/java/dao/BookDAOImpl.java
@@ -10,12 +10,12 @@ import java.util.List;
 import model.Book;
 
 public class BookDAOImpl implements BookDAO {
-    private static String DELETE_BOOK = "DELETE FROM Sach WHERE id = ?";
-    private static String UPDATE_BOOK = "UPDATE Sach SET name = ?, author = ?, quantity = ?, price = ? WHERE id = ?";
-    private static String SEARCH_BY_NAME = "SELECT * FROM Sach WHERE name LIKE ?";
-    private static String GET_BOOK_BY_ID = "SELECT * FROM Sach WHERE id = ?";
-    private static String SELECT_ALL = "SELECT * FROM Sach";
-    private static String ADD_BOOK = "INSERT INTO Sach (name, author, quantity, price) VALUES (?, ?, ?, ?)";
+    private static String DELETE_BOOK = "DELETE FROM Book WHERE id = ?";
+    private static String UPDATE_BOOK = "UPDATE Book SET name = ?, author = ?, quantity = ?, price = ? WHERE id = ?";
+    private static String SEARCH_BY_NAME = "SELECT * FROM Book WHERE name LIKE ?";
+    private static String GET_BOOK_BY_ID = "SELECT * FROM Book WHERE id = ?";
+    private static String SELECT_ALL = "SELECT * FROM Book";
+    private static String ADD_BOOK = "INSERT INTO Book (name, author, quantity, price) VALUES (?, ?, ?, ?)";
 
     @Override
     public List<Book> getAllBook() {
