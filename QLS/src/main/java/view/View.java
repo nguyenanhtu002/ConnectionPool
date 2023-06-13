c632c3070417e322fe0737aa3508f43a4080ba69
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-06-13 10:30:45 +0700

    Add files via upload

---
+++ QLS/src/main/java/view/View.java
@@ -0,0 +1,51 @@
+package view;
+
+
+import controller.BookController;
+
+import java.util.Scanner;
+
+public class View {
+    private static BookController bookController = new BookController();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. Add Book");
+            System.out.println("2. Update Book");
+            System.out.println("3. Delete Book");
+            System.out.println("4. Search by Name");
+            System.out.println("5. Search by Id");
+            System.out.println("6. Show Book");
+            System.out.println("7. Exit");
+            System.out.print("Enter Your Choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 1:
+                    bookController.addBook();
+                    break;
+                case 2:
+                    bookController.updateBook();
+                    break;
+                case 3:
+                    bookController.deleteBook();
+                    break;
+                case 4:
+                    bookController.searchBookByName();
+                    break;
+                case 5:
+                    bookController.getBookById();
+                    break;
+                case 6:
+                    bookController.getAllBook();
+                    break;
+                case 7:
+                    System.exit(0);
+                default:
+                    System.out.println("Try Again");
+            }
+        }
+
+    }
+}
