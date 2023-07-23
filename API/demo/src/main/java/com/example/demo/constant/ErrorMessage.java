59021e525d76daffa0da16d62a3fb23d635cc1cf
nguyenanhtu002 <132594211+nguyenanhtu002@users.noreply.github.com>
2023-07-23 10:10:35 +0700

    Delete API/demo directory

--- API/demo/src/main/java/com/example/demo/constant/ErrorMessage.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.constant;
-
-public class ErrorMessage {
-    public static final String EXISTED = "Exited in database";
-    public static final String NOT_FOUND = "Not found";
-}
--- API/demo/src/main/java/com/example/demo/constant/MessageResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.constant;
-
-public class MessageResponse {
-    public final static String SUCCESS = "Success";
-    public final static String INF_TAG = "TagFinance InFor";
-    public final static String INF_TRAN = "Transaction InFor";
-}
